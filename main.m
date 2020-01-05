14920f2fd5212fe65c8e0b9e94f11df6c6b039ea
enesaktepe78 <32266858+enesaktepe78@users.noreply.github.com>
2020-01-05 05:12:17 +0300

    Add files via upload

---
+++ main.m
@@ -0,0 +1,60 @@
+load images.mat
+% take the first image
+firstImage = inpM(:,:,1);
+filter = [1,0;
+          0,1];
+      
+conv2ed = conv2(firstImage,filter,'valid');
+%cleared = clearPadding(conv2ed);
+
+dlX = dlarray(conv2ed, 'SSCB');
+
+[pooling_Matrix, indx, dataSize] = maxpool(dlX,4,'Stride',4);
+
+y = extractdata(pooling_Matrix);
+flattening = reshape(y,[],1);
+[sizeOf, temp] = size(flattening); 
+%weights = rand(3, sizeOf);
+weights=ones(3,sizeOf)/10;
+
+Vs = weights * flattening;
+softmaxVs = softmax(Vs);
+
+%Backpropagation
+S=sum(softmaxVs);
+grad(1)=-1*(-1/softmaxVs(1))*((exp(softmaxVs(1))*exp(softmaxVs(2))*exp(softmaxVs(3))/S^3));
+grad(2)=-1*(-1/softmaxVs(1))*((exp(softmaxVs(1))*exp(softmaxVs(2)))/S^2);
+grad(3)=-1*(-1/softmaxVs(1))*((exp(softmaxVs(1))*exp(softmaxVs(2)))/S^2);
+
+%finding local gradient flattening
+for i=1:sizeOf
+    F_gradient(i)=weights(1,i)*grad(1)+weights(2,i)*grad(2)+weights(3,i)*grad(3);
+end
+
+%updating weights
+w_up=grad'.*flattening'*(1/10)+weights; 
+
+%writing flatteing gradients to indices
+B=reshape(F_gradient,12,12);
+C=dlarray(B, 'SSCB');
+dlY = maxunpool(C,indx,dataSize);
+%rotating  Cs
+M90=rot90(dlY);
+M180=rot90(M90);
+
+%finding delta filter
+delta_filter=conv2(firstImage,extractdata(M180),'valid');
+
+%updating filter
+update_filter=filter+(1/10)*delta_filter;
+
+%deneme amaçlý
+asdf=conv2(firstImage,update_filter,'valid');
+
+
+
+
+
+
+
+
